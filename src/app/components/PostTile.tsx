58c9fa0e1177b358f40b7b2980a7e74e2a14249e
wojnet <wojnetto@gmail.com>
2024-04-13 19:58:53 +0200

    Fixed PostTile Image

--- src/app/components/PostTile.tsx
+++ src/app/components/PostTile.tsx
@@ -15,16 +15,18 @@ interface PostTileProps {
 const PostTile: FC<PostTileProps> = ({ title, summary, imageSrc, imageAlt, author, creationDate }) => {
     const localCreationDate = new Date(creationDate).toLocaleDateString("en-EN", { day: "2-digit", month: "long", year: "numeric" });
 
+    console.log("src:", imageSrc);
+
     return (
         <Link href={`/post/${title.replaceAll(" ", "_")}`}>
             <div className="w-[300px] h-auto flex flex-col items-center cursor-pointer p-[12px] hover:scale-[0.98] transition group"> {/* hover:outline hover:outline-[1px] hover:outline-gray-text hover:rounded-[30px] */}
-                <img 
-                    src={imageSrc} 
+                <Image
+                    src={"https:" + imageSrc} 
                     alt={imageAlt}
                     width={0}
                     height={0}
-                    placeholder="blur"
                     className="w-full h-[220px] object-cover rounded-[25px] mb-[20px] shadow-lg transition" 
+                    unoptimized
                 />
                 <h3 className="text-center text-[19px] font-bold mb-[15px] leading-[22px] group-hover:text-color-2 transition">{ title }</h3>
                 <p className="text-gray-text text-center m-0 line-clamp-[10] leading-[21px] mb-[15px] group-hover:text-color-4 transition">{ summary }</p>
